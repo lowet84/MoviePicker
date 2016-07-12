5e4e9aa783d2b74fbf8f55449ead6bd7e71263cf
Fredrik Löwenhamn <lowet84@gmail.com>
2016-07-12 15:39:01 +0200

    Add project files.

---
+++ src/MoviePicker/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using Microsoft.AspNet.Mvc;
+using MoviePicker.Model;
+using MoviePicker.Utility;
+
+namespace MoviePicker.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<JackettResultGroup> Get()
+        {
+            var ret = JackettHelper.Query(string.Empty);
+            return ret.Take(10);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IEnumerable<JackettResultGroup> Get(string id)
+        {
+            var ret = JackettHelper.Query(id);
+            return ret;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public bool Post([FromBody]PostData data)
+        {
+            return DelugeHelper.Download(data.Link, data.Pass);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        public class PostData
+        {
+            public string Link { get; set; }
+            public string Pass { get; set; }
+        }
+    }
+}
---
+++ src/MoviePicker/Model/JackettResult.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviePicker.Model
+{
+    public class JackettResult
+    {
+        public string ReleaseTitle { get; set; }
+        public int Seeders { get; set; }
+
+        public int Year { get; set; }
+
+        public string Name { get; set; }
+        public string Quality { get; set; }
+        public string Link { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}[{Year}] ({Seeders}) {Quality}";
+        }
+    }
+
+    public class JackettResultGroup
+    {
+        public string Name { get; }
+        public int Year { get; }
+        public IEnumerable<JackettResult> Results { get; }
+
+        public JackettResultGroup(string name, int year, IEnumerable<JackettResult> results)
+        {
+            Name = name;
+            Year = year;
+            Results = results;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Results.Count()}) [{Results.Sum(d=>d.Seeders)}]";
+        }
+    }
+}
