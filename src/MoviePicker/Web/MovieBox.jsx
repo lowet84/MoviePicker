d4244d61bb24401d187f8afe052bed650a86f993
Fredrik Löwenhamn <lowet84@gmail.com>
2016-07-15 15:46:48 +0200

    More fixes!

--- src/MoviePicker/Web/MovieBox.jsx
+++ src/MoviePicker/Web/MovieBox.jsx
@@ -1,28 +1,51 @@
 ﻿export default React.createClass({
-    getInitialState: function() {
+    getInitialState: function () {
         return {data: []};
     },
-    componentDidMount: function() {
+    expand: function (index) {
+        var data = this.state.data;
+        data[index].expanded = data[index].expanded != true;
+
+        this.setState({data: data});
+    },
+    componentDidMount: function () {
         $.ajax({
             url: this.props.url,
             dataType: 'json',
             cache: false,
-            success: function(data) {
+            success: function (data) {
                 this.setState({data: data});
             }.bind(this),
-            error: function(xhr, status, err) {
+            error: function (xhr, status, err) {
                 console.error(this.props.url, status, err.toString());
             }.bind(this)
         });
     },
     render: function () {
-        var movies = this.state.data.map(function (item) {
-            return (
-                <p key={item.Name+'-'+item.Year}>
-                    {item.Name} ({item.Year}) {item.Seeders}
-                </p>
-            );
-        });
+        var movies = this.state.data.map(function (item, index) {
+            if (item.expanded == true) {
+                var children = item.Results.map(function (item, index) {
+                    return (
+                        <a key={this.index + "_" + index}>[{item.ReleaseTitle}]</a>
+                    );
+                }, this);
+                return (
+                    <div key={index}>
+                        <p onClick={()=>this.expand(index)}>
+                            {item.Name} ({item.Year}) {item.Seeders}
+                        </p>
+                        {children}
+                    </div>
+                );
+            }
+            else {
+                return (
+                    <p key={index} onClick={()=>this.expand(index)}>
+                        {item.Name} ({item.Year}) {item.Seeders}
+                    </p>
+                );
+            }
+        }, this);
         return (
             <div>
                 <h1>Top Movies!</h1>
